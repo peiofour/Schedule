20660084be8b752c9c2c56fce36c7325095bf4e5
Pierre Fournier <pierre.fournier@ynov.com>
2020-05-06 11:58:23 +0200

    Updating Child class, using now a List<Task> for task list/planning

--- app/src/main/java/com/pierrefournier/schedule/model/Child.java
+++ app/src/main/java/com/pierrefournier/schedule/model/Child.java
@@ -1,12 +1,19 @@
 package com.pierrefournier.schedule.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Child extends User {
 
-    private Planning planning;
+    private final List<Task> taskList;
 
     public Child(String firstName, String lastName, String pseudo, String password, String phone) {
 
         super(firstName, lastName, pseudo, password, phone, false);
-        this.planning = new Planning();
+        taskList = new ArrayList<Task>();
+    }
+
+    public List<Task> getTaskList() {
+        return taskList;
     }
 }
